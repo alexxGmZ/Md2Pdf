d67d6261374aba421708f39815f77b922fd444bf
alexxGmZ <algilbertgomez@gmail.com>
2024-11-18 20:33:29 +0800

    refactor: check for specific pandoc process instead of pdf engine

--- lua/Md2Pdf/init.lua
+++ lua/Md2Pdf/init.lua
@@ -10,11 +10,11 @@ local function notify(message, log_level)
    return vim.notify(message, log_level, { title = "Md2Pdf" })
 end
 
---- check if the pdf engine process is running
----@param pdf_engine string
+--- Check if the specific pandoc process is running
+---@param pandoc_process string
 ---@return boolean
-local function is_pdf_engine_running(pdf_engine)
-   local command = { "pgrep", pdf_engine }
+local function is_pandoc_running(pandoc_process)
+   local command = { "pgrep", "-f", pandoc_process }
    local obj = vim.system(command, { text = true }):wait()
 
    if obj.stdout ~= "" then
@@ -36,15 +36,10 @@ local function start(config)
          local md_file = vim.api.nvim_buf_get_name(0)
          local pdf_file = md_file:gsub("%.md$", ".pdf")
          local command = { "pandoc", md_file, "--pdf-engine=" .. config.pdf_engine, "-o", pdf_file }
-         local notify_table_data = {
-            "Pdf Engine: ", config.pdf_engine, "\n",
-            "md        : ", md_file, "\n",
-            "pdf       : ", pdf_file
-         }
 
          -- kill the previous process to finish the latest process
-         if is_pdf_engine_running(config.pdf_engine) then
-            local process_to_kill = "pandoc " .. md_file
+         local process_to_kill = "pandoc " .. md_file
+         if is_pandoc_running(process_to_kill) then
             vim.system({ "pkill", "-f", process_to_kill }):wait()
          end
 
