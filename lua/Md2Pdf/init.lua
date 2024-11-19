00a1ecb9a758fba77441c8b2ae1a91f23e437761
alexxGmZ <algilbertgomez@gmail.com>
2024-11-19 13:30:00 +0800

    refactor: use jobstart() and jobstop()
    Lessen the outside dependencies by utizling vim.fn.jobstart() and
    vim.fn.jobstop() instead of unix's pkill and pgrep

--- lua/Md2Pdf/init.lua
+++ lua/Md2Pdf/init.lua
@@ -10,24 +10,12 @@ local function notify(message, log_level)
    return vim.notify(message, log_level, { title = "Md2Pdf" })
 end
 
---- Check if the specific pandoc process is running
----@param pandoc_process string
----@return boolean
-local function is_pandoc_running(pandoc_process)
-   local command = { "pgrep", "-f", pandoc_process }
-   local obj = vim.system(command, { text = true }):wait()
-
-   if obj.stdout ~= "" then
-      return true
-   end
-
-   return false
-end
-
 --- Start the auto command
 ---@param config table
 local function start(config)
    if autocmd_id then return end
+
+   local job_id
 
    autocmd_id = vim.api.nvim_create_autocmd("BufWritePost", {
       pattern = "*.md",
@@ -37,17 +25,21 @@ local function start(config)
          local pdf_file = md_file:gsub("%.md$", ".pdf")
          local command = { "pandoc", md_file, "--pdf-engine=" .. config.pdf_engine, "-o", pdf_file }
 
-         -- kill the previous process to finish the latest process
-         local process_to_kill = "pandoc " .. md_file
-         if is_pandoc_running(process_to_kill) then
-            vim.system({ "pkill", "-f", process_to_kill }):wait()
+         -- kill the previous job to finish the latest job
+         if job_id then
+            vim.fn.jobstop(job_id)
          end
 
-         vim.system(command, { text = true }, function(obj)
-            if obj.stderr ~= "" then
-               return notify(obj.stderr, "WARN")
+         -- start a job
+         job_id = vim.fn.jobstart(command, {
+            detach = true, -- keep converting even if nvim is closed
+            on_stderr = function(_, data)
+               local err_msg = data and table.concat(data, " ")
+               if err_msg ~= "" then
+                  return notify(err_msg, "WARN")
+               end
             end
-         end)
+         })
       end
    })
 end
