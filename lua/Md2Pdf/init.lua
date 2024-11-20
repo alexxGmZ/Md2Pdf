fce427a6d989aa9d298a74d091df644bfaf6355b
alexxGmZ <algilbertgomez@gmail.com>
2024-11-20 12:20:30 +0800

    feat: manual conversion

--- lua/Md2Pdf/init.lua
+++ lua/Md2Pdf/init.lua
@@ -1,4 +1,5 @@
 local default_config = require("Md2Pdf.config").default_config
+local config = {}
 local autocmd_id
 local M = {}
 local buffer_job = {}
@@ -11,9 +12,57 @@ local function notify(message, log_level)
    return vim.notify(message, log_level, { title = "Md2Pdf" })
 end
 
+--- Convert markdown to pdf job
+---@param md_file string
+local function convert_md(md_file)
+   local pdf_file = md_file:gsub("%.md$", ".pdf")
+   local command = { "pandoc", md_file, "--pdf-engine=" .. config.pdf_engine, "-o", pdf_file }
+
+   -- create a specific job for each buffer with the markdown filename as the key
+   -- each buffer has job_id
+   if not buffer_job[md_file] then
+      buffer_job[md_file] = { job_id = nil }
+   end
+
+   if not buffer_job[md_file].job_id then
+      notify("Converting...")
+   end
+
+   -- kill the previous buffer job to finish its latest job
+   if buffer_job[md_file].job_id then
+      vim.fn.jobstop(buffer_job[md_file].job_id)
+      buffer_job[md_file].job_id = nil
+      notify("Re-converting...")
+   end
+
+   -- start conversion job
+   buffer_job[md_file].job_id = vim.fn.jobstart(command, {
+      detach = true,       -- keep converting even if nvim is closed
+      stderr_buffered = true,
+      on_stderr = function(_, data)
+         local err_msg = table.concat(data, "\n")
+         if err_msg ~= "" then
+            buffer_job[md_file].job_id = nil       -- delete buffer job id if failed
+            notify(err_msg, "WARN")
+         end
+      end,
+      on_exit = function(_, code)
+         if code == 0 then
+            buffer_job[md_file].job_id = nil       -- delete buffer job id if failed
+
+            local success_message = {
+               "Pdf Engine: ", config.pdf_engine, "\n",
+               "md        : ", md_file, "\n",
+               "pdf       : ", pdf_file
+            }
+            notify(table.concat(success_message))
+         end
+      end
+   })
+end
+
 --- Start the auto command
----@param config table
-local function start(config)
+local function start()
    if autocmd_id then return end
 
    autocmd_id = vim.api.nvim_create_autocmd("BufWritePost", {
@@ -21,50 +70,8 @@ local function start(config)
       group = vim.api.nvim_create_augroup("Md2Pdf", {}),
       callback = function()
          local md_file = vim.api.nvim_buf_get_name(0)
-         local pdf_file = md_file:gsub("%.md$", ".pdf")
-         local command = { "pandoc", md_file, "--pdf-engine=" .. config.pdf_engine, "-o", pdf_file }
-
-         -- create a specific job for each buffer with the markdown filename as the key
-         -- each buffer has job_id
-         if not buffer_job[md_file] then
-            buffer_job[md_file] = { job_id = nil }
-         end
-
-         if not buffer_job[md_file].job_id then
-            notify("Converting...")
-         end
-
-         -- kill the previous buffer job to finish its latest job
-         if buffer_job[md_file].job_id then
-            vim.fn.jobstop(buffer_job[md_file].job_id)
-            buffer_job[md_file].job_id = nil
-            notify("Re-converting...")
-         end
-
-         -- start conversion job
-         buffer_job[md_file].job_id = vim.fn.jobstart(command, {
-            detach = true, -- keep converting even if nvim is closed
-            stderr_buffered = true,
-            on_stderr = function(_, data)
-               local err_msg = table.concat(data, "\n")
-               if err_msg ~= "" then
-                  buffer_job[md_file].job_id = nil -- delete buffer job id if failed
-                  notify(err_msg, "WARN")
-               end
-            end,
-            on_exit = function(_, code)
-               if code == 0 then
-                  buffer_job[md_file].job_id = nil -- delete buffer job id if failed
-
-                  local success_message = {
-                     "Pdf Engine: ", config.pdf_engine, "\n",
-                     "md        : ", md_file, "\n",
-                     "pdf       : ", pdf_file
-                  }
-                  notify(table.concat(success_message))
-               end
-            end
-         })
+         convert_md(md_file)
+         -- vim.print(vim.inspect(buffer_job))
       end
    })
 end
@@ -82,20 +89,23 @@ function M.setup(opts)
    vim.api.nvim_create_user_command("Md2Pdf", function(args)
       local arg = args.fargs[1] or ""
 
-      if arg == "stop" then
-         return stop()
-      end
-
-      local config = default_config
+      config = default_config
       if opts and next(opts) then
          config = opts
       end
 
-      start(config)
+      if arg == "stop" then
+         return stop()
+      elseif arg == "convert" then
+         local md_file = vim.api.nvim_buf_get_name(0)
+         return convert_md(md_file)
+      end
+
+      start()
    end, {
       nargs = "*",
       complete = function()
-         return { "start", "stop" }
+         return { "start", "stop", "convert" }
       end
    })
 end
